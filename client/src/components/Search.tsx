4e9b12befb871cadfb5aeb3b76864f947607a392
Will DeBernardi <will@debernardi.us>
2022-04-18 15:03:01 -0600

    Added completely non-functioning search bar

---
+++ client/src/components/Search.tsx
@@ -0,0 +1,27 @@
+import { Box, TextField, Typography, InputAdornment } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+
+function Search() {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", width: "60vw" }}>
+      <Typography variant="h1">Female Au Ra {">>>>>"}</Typography>
+      <form>
+        <TextField
+          id="search"
+          type="search"
+          placeholder="Pee pee poo poo"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>),
+          }}
+        />
+      </form>
+    </Box>
+  );
+}
+
+export default Search;
