cc9a2baa2c46f1fae8888a8a6c289f0be56157cc
Will DeBernardi <will@debernardi.us>
2022-05-17 09:44:40 -0600

    Remove broken import

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
 import { CssBaseline } from "@mui/material";
-import { useState } from "react";
-import axios from "axios";
 import Search from "./components/Search/Search";
-import Results from "./components/Search/Results";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Header from "./components/Header/Header";
 
