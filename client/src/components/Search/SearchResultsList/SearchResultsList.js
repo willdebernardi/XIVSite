1fed796c8a3732f2da8d7e11d4b2af7ea46cfe90
Jonathan Salazar <jonsalaz23@gmail.com>
2022-06-19 09:45:32 -0600

    Render SearchResultsList when no exact match found

--- client/src/components/Search/SearchResultsList/SearchResultsList.js
+++ client/src/components/Search/SearchResultsList/SearchResultsList.js
@@ -3,12 +3,15 @@ import Grid from "@mui/material/Grid";
 import ResultsCard from "./ResultsCard";
 
 function SearchResultsList(props) {
+    console.log(props.results);
     let resultsCards = props.results.map(item => {
-        return <ResultsCard item={item} />
+        if(item.UrlType.toLowerCase() === "item") {
+            return <ResultsCard item={item} />
+        }
     })
 
     return (
-        <Grid container columns={3} spacing={1}>
+        <Grid container columns={3} spacing={1} sx= {{padding: "10px"}}>
             {resultsCards}
         </Grid>
     )
