038669bf53d694b4cc17f3ef56d3bb118d571e0a
Jonathan Salazar <jonsalaz23@gmail.com>
2022-06-21 17:14:46 -0600

    Refactored API request to SearchResultsList

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -8,47 +8,23 @@ import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
 import "./Search.css";
 import { useState } from "react";
-import axios from "axios";
-import SearchResultsList from "./SearchResultsList/SearchResultsList";
 import { useNavigate } from "react-router-dom";
 
 function Search() {
     const navigate = useNavigate();
 
-    const [didSearch, setDidSearch] = useState(false);
-    const [results, setResults] = useState();
     const [val, setVal] = useState("");
 
     const searchItemID = (event) => {
         event.preventDefault();
+
         let searchItem = val.toLowerCase();
-
-        axios
-            .get(`https://xivapi.com/search?string=${searchItem}`)
-            .then((res) => {
-                setResults(res.data.Results);
-                const keys = Object.keys(res.data.Results);
-                for (var i = 0; i < keys.length; i++) {
-                    let item = res.data.Results[keys[i]];
-                    if (
-                        item.Name.toLowerCase() === searchItem &&
-                        item.UrlType.toLowerCase() === "item"
-                    ) {
-                        navigate(`/item/${item.ID}`);
-                    }
-                }
-
-                setDidSearch(true);
-            });
+        navigate(`/search/${searchItem}`)
     };
 
-    if (didSearch) {
-        return <SearchResultsList results={results} />;
-    } else {
-        return (
-            <SearchBox submitFunc={searchItemID} val={val} setVal={setVal} />
-        );
-    }
+    return (
+        <SearchBox submitFunc={searchItemID} val={val} setVal={setVal} />
+    );
 }
 
 function SearchBox(props) {
