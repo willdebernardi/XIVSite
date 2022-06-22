dec6d794932a09d014d387bdf716c17c3321069a
Jonathan Salazar <jonsalaz23@gmail.com>
2022-06-22 11:16:11 -0600

    Fixed Search Results not displaying

--- client/src/components/Search/SearchResultsList/SearchResultsList.js
+++ client/src/components/Search/SearchResultsList/SearchResultsList.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { Typography } from "@mui/material";
 
 function SearchResultsList() {
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState(null);
     const [firstRender, setFirstRender] = useState(true);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -35,31 +35,30 @@ function SearchResultsList() {
                     item.Name.toLowerCase() === params.name &&
                     item.UrlType.toLowerCase() === "item"
                 ) {
-                    navigate(`/item/${item.ID}`, {replace: true});
+                    navigate(`/item/${item.ID}`, { replace: true });
                 }
             }
             setIsLoading(false);
         }
     }, [results]);
 
-    let resultsCards = () => {
-        console.log(results.length);
-        if(results.length === 0) {
-            return <Grid item>
-                    <Typography variant="h2">No Results Found.</Typography>
+    return (
+        <Grid
+            container
+            columns={3}
+            spacing={1}
+            justifyContent={"center"}
+            sx={{ padding: "10px" }}
+        >
+            <Grid item xs={3} textAlign={"center"}>
+                Hello World!
+            </Grid>
+            {!results && (
+                <Grid item component={"h2"} textAlign={"center"}>
+                    Loading...
                 </Grid>
-        }
-        results.map((item) => {
-            if (item.UrlType.toLowerCase() === "item") {
-                return <ResultsCard item={item} />;
-            }
-        });
-    }
-
-    return ( 
-        isLoading ? <h1>Loading...</h1> :
-        <Grid container columns={3} spacing={1} sx={{ padding: "10px" }}>
-            {resultsCards()}
+            )}
+            {results && results.map((item) => <ResultsCard item={item} />)}
         </Grid>
     );
 }
